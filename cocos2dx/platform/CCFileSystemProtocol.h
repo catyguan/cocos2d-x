ffd928c44fac0ba064c873ae69f3dc8c55deae6d
guan zhong <catyguan@gmail.com>
2013-09-09 19:53:37 +0800

    FileSystem

---
+++ cocos2dx/platform/CCFileSystemProtocol.h
@@ -0,0 +1,74 @@
+#ifndef __CC_FILESYSTEM_PROTOCOL_H__
+#define __CC_FILESYSTEM_PROTOCOL_H__
+
+#include <string>
+#include <vector>
+#include <map>
+#include "CCPlatformMacros.h"
+#include "ccTypes.h"
+#include "ccTypeInfo.h"
+
+NS_CC_BEGIN
+
+enum FileSystemType
+{	
+	kAll,
+    kResource,
+	kAppData,   
+	kUserData,
+    kLua
+};
+
+class CC_DLL CCFileSystemProtocol
+{
+public:
+	virtual bool fileExists(FileSystemType type, const char* pszFileName) = 0;
+	virtual unsigned char* fileRead(FileSystemType type, const char* pszFileName, unsigned long* pSize) = 0;
+	virtual unsigned long fileWrite(FileSystemType type, const char* pszFileName, unsigned char* content, long size) = 0;
+	virtual bool fileDelete(FileSystemType type, const char* pszFileName) = 0;
+
+	virtual void reset() = 0;    
+};
+
+typedef struct _FS4NItem {
+	FileSystemType type;
+	std::string path;
+	bool readonly;
+} FS4NItem;
+
+class CC_DLL CCFileSystemBase : public CCFileSystemProtocol
+{
+public:   
+    static CCFileSystemBase* sharedFileSystem();
+        
+	CCFileSystemBase();
+    virtual ~CCFileSystemBase();
+
+	void install();
+
+	virtual bool fileExists(FileSystemType type, const char* pszFileName);
+	virtual unsigned char* fileRead(FileSystemType type, const char* pszFileName, unsigned long* pSize);
+	virtual unsigned long fileWrite(FileSystemType type, const char* pszFileName, unsigned char* content, long size);
+	virtual bool fileDelete(FileSystemType type, const char* pszFileName);
+    
+	virtual void reset();    
+	
+	virtual std::string getPath(const FS4NItem* info, std::string name);
+
+	void addSearchPath(FileSystemType type, const char* path, bool readonly);
+
+protected:
+	virtual bool fileExists(const char* pszFileName) = 0;
+	virtual unsigned char* fileRead(const char* pszFileName, unsigned long* pSize) = 0;
+	virtual unsigned long fileWrite(const char* pszFileName, unsigned char* content, long size) = 0;
+	virtual bool fileDelete(const char* pszFileName) = 0;
+    
+protected:
+    std::vector<FS4NItem> m_searchPaths;
+    
+    static CCFileSystemBase* s_sharedFileSystem;    
+};
+
+NS_CC_END
+
+#endif    // __CC_FILESYSTEM_PROTOCOL_H__
---
+++ cocos2dx/platform/win32/CCFileSystemWin32.cpp
@@ -0,0 +1,62 @@
+#include "CCFileSystemWin32.h"
+
+NS_CC_BEGIN
+
+CCFileSystemWin32::CCFileSystemWin32()
+{
+}
+
+CCFileSystemWin32::~CCFileSystemWin32()
+{
+}
+
+bool CCFileSystemWin32::fileExists(const char* vpathName)
+{
+	DWORD fa = ::GetFileAttributesA(vpathName);
+	return fa != INVALID_FILE_ATTRIBUTES;
+}
+
+unsigned char* CCFileSystemWin32::fileRead(const char* vpathName, unsigned long* pSize)
+{
+	int size = 0;
+	unsigned char* buf = NULL;
+	do {
+		// read the file from hardware
+		FILE *fp = fopen(vpathName, "rb");
+		CC_BREAK_IF(!fp);
+        
+		fseek(fp,0,SEEK_END);
+		size = ftell(fp);
+		fseek(fp,0,SEEK_SET);
+		buf = new unsigned char[size];
+		fread(buf,sizeof(char), size,fp);
+		fclose(fp);
+    } while (0);
+    
+    if (! buf)
+    {
+        return NULL;
+    }    
+	*pSize = size;
+	return buf;
+}
+
+unsigned long CCFileSystemWin32::fileWrite(const char* pszFileName, unsigned char* content, long size)
+{
+	FILE *fp = fopen(pszFileName, "wb");
+	bool r = false;
+	size_t wcount = 0;
+	if(fp) {
+		wcount = fwrite(content, size,1, fp);
+		fclose(fp);	
+	}
+	return wcount;
+}
+
+bool CCFileSystemWin32::fileDelete(const char* pszFileName)
+{
+	return ::DeleteFileA(pszFileName)==TRUE;
+}
+
+NS_CC_END
+
